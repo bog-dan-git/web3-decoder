22347a0c56ee32f2757602098e6eec249df539eb
bog-dan-git <43274641+bog-dan-git@users.noreply.github.com>
2024-03-31 16:18:12 +0300

    feat: support singular requests, add empty page message

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-import React, { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import NetworkItem from './components/network-item/NetworkItem';
 import './App.css';
 import OnChainDataProvider from './components/OnChainDataProvider';
 import { JsonRpcRequest, JsonRpcResponse } from './interfaces';
 
-const isSupportedRequest = (request: unknown) =>
-  Array.isArray(request) &&
-  request.every((x) => x.jsonrpc === '2.0' && x.method.startsWith('eth_'));
+const isSupportedRequest = (request: JsonRpcRequest | JsonRpcRequest[]) =>
+  Array.isArray(request)
+    ? request.every((x) => x.jsonrpc === '2.0' && x.method.startsWith('eth_'))
+    : request.jsonrpc === '2.0' && request.method.startsWith('eth_');
+
+interface RequestItem {
+  url: string;
+  request: JsonRpcRequest[];
+  response: JsonRpcResponse[];
+}
 
 function App() {
+  const [requests, setRequests] = useState<RequestItem[]>([]);
+
   const insertRequest = (
     url: string,
     request: JsonRpcRequest[],
     response: JsonRpcResponse[],
   ) => {
     setRequests((requests) => {
-      return [...requests, { url, request, response }];
+      return [
+        ...requests,
+        {
+          url,
+          request: Array.isArray(request) ? request : [request],
+          response: Array.isArray(response) ? response : [response],
+        },
+      ];
     });
   };
-  const [requests, setRequests] = React.useState(
-    [] as {
-      url: string;
-      request: JsonRpcRequest[];
-      response: JsonRpcResponse[];
-    }[],
-  );
 
   const handleRequestCallback = useCallback(
     (request: chrome.devtools.network.Request) => {
@@ -58,21 +67,30 @@ function App() {
         handleRequestCallback,
       );
     };
-  });
+  }, [handleRequestCallback]);
+
   return (
     <OnChainDataProvider>
       <div className="app">
-        <h1>Requests</h1>
-        <div className="requests">
-          {requests.map((request, index) => (
-            <NetworkItem
-              key={index}
-              request={request.request}
-              response={request.response}
-              url={request.url}
-            />
-          ))}
-        </div>
+        {requests.length ? (
+          <div className="requests">
+            {requests.map((request, index) => (
+              <NetworkItem
+                key={index}
+                request={request.request}
+                response={request.response}
+                url={request.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="center">
+            <div>Recording web3 activity...</div>
+            <div>
+              Perform a request or hit <b>Ctrl + R</b> to record the reload
+            </div>
+          </div>
+        )}
       </div>
     </OnChainDataProvider>
   );
